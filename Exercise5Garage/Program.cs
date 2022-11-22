0136c1a3b6adb613796dd797a844e7ea8d416444
Jonas Lundberg <jonlu8777@gmail.com>
2022-11-22 15:56:30 +0100

    Add project files.

---
+++ Exercise5Garage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise5Garage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
